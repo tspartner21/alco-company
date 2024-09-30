76fedcdae3d8a04b07b0c30575c654c1ce995dab
tubi55 <tubi55@nate.com>
2024-09-30 16:45:59 +0900

    브라우저 리사이즈시 현재 활성화된 버튼 순번으로 스크롤 위치값을 바로바로 갱신처리

--- js/main.js
+++ js/main.js
@@ -14,13 +14,12 @@ getPosArr(secArr);
 //브라우저 리사이즈 이벤트 발생할 때마다 새로운 값으로 갱신
 window.addEventListener("resize", () => {
 	getPosArr(secArr);
+	modifyPos();
 });
 
 //scroll btn evnet
 scroll_btns.forEach((btn, idx) => {
-	btn.addEventListener("click", () => {
-		new Anime(window, { scroll: posArr[idx] });
-	});
+	btn.addEventListener("click", () => moveScroll(idx));
 });
 
 //window scroll event
@@ -40,8 +39,22 @@ function activation(arrEl, index) {
 
 //get box position array func
 function getPosArr(arrEl) {
-	//일단은 기존 posArr값을 비운뒤
-	//새로운 새로 위치값을 배열에 담아줌
 	posArr = [];
 	for (let el of arrEl) posArr.push(el.offsetTop);
+}
+
+//move scroll
+function moveScroll(index, speed = 500) {
+	new Anime(window, { scroll: posArr[index] }, { duration: speed });
+}
+
+//when resize modifying scroll position
+//현재 활성화된 버튼의 순번 위치로 모션없이 바로 스크롤 이동처리
+function modifyPos() {
+	const activeEl = document.querySelector("li.on");
+	//Array.from(유사배열) - 유사배열을 순수배열 형태로 변환해서 반환
+	//전체배열.indexOf(특정배열) : 전체배열에서 특정배열값의 순번을 반환
+	//scroll_btns라는 버튼 그룹에서 on클래스가 붙어있는 버튼의 순서값을 반환
+	const activeIndex = Array.from(scroll_btns).indexOf(activeEl);
+	moveScroll(activeIndex, 0);
 }
