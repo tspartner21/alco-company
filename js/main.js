1065f645671758a9e4f05604bc19b76f1030bb63
tubi55 <tubi55@nate.com>
2024-09-30 18:29:10 +0900

    modifyPos함수 호출시 자체적으로 getPosArr함수 자동 실행

--- js/main.js
+++ js/main.js
@@ -10,18 +10,22 @@ const scroll_btns = document.querySelectorAll(".scroller li");
 getPosArr(secArr);
 
 //get new position array whenever scrolls
-window.addEventListener("resize", () => {
-	getPosArr(secArr);
-	modifyPos();
-});
+window.addEventListener("resize", modifyPos);
 
 //scroll btn evnet
-scroll_btns.forEach((btn, idx) => {
-	btn.addEventListener("click", () => moveScroll(idx));
-});
+scroll_btns.forEach((btn, idx) => btn.addEventListener("click", () => moveScroll(idx)));
 
 //window scroll event
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", activateScroll);
+
+//get box position array func
+function getPosArr(arrEl) {
+	posArr = [];
+	for (let el of arrEl) posArr.push(el.offsetTop);
+}
+
+//scroll activation func
+function activateScroll() {
 	posArr.forEach((_, idx) => {
 		const condition =
 			idx !== posArr.length - 1
@@ -32,12 +36,6 @@ window.addEventListener("scroll", () => {
 			? [scroll_btns, secArr].forEach(arr => arr[idx].classList.add("on"))
 			: [scroll_btns, secArr].forEach(arr => arr[idx].classList.remove("on"));
 	});
-});
-
-//get box position array func
-function getPosArr(arrEl) {
-	posArr = [];
-	for (let el of arrEl) posArr.push(el.offsetTop);
 }
 
 //move scroll
@@ -47,6 +45,9 @@ function moveScroll(index, speed = 500) {
 
 //when resize modifying scroll position
 function modifyPos() {
+	//modifyPos가 호출될떄 어차피 getPosArr로 실행되야 하므로
+	//해당 함수 안쪾에 자체적으로 getPosArr호출
+	getPosArr(secArr);
 	const activeEl = document.querySelector("li.on");
 	const activeIndex = Array.from(scroll_btns).indexOf(activeEl);
 	moveScroll(activeIndex, 0);
