8cd25f25ca0345040bc6ba4bd173571b84f74756
tubi55 <tubi55@nate.com>
2024-09-30 11:29:10 +0900

    scrollY, offsetTop 개념 정리

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 import Anime from "./anime.js";
 
-const h1 = document.querySelector("header h1");
+const scroll_btns = document.querySelectorAll(".scroller li");
+const banner = document.querySelector("#banner");
 
-h1.addEventListener("click", e => {
-	e.preventDefault();
-	new Anime(window, { scroll: 700 }, { duration: 1000 });
+//scroller click event bind
+scroll_btns.forEach((btn, idx) => {
+	btn.addEventListener("click", () => {
+		console.log(idx);
+	});
 });
+
+//window scroll event bind
+window.addEventListener("scroll", () => {
+	console.log("scrollY", window.scrollY);
+	console.log("offsetTop", banner.offsetTop);
+});
+
+/*
+	window.scrollY : 현재 브라우저에서 스크롤된 거리값 (동적)
+	DOM.offsetTop : 문서 상단 끝에서부터 현재 돔의 세로 위치값 (정적)
+
+	미션 
+	- 이동하려고 하는 4개 영역의 세로 위치값을 배열로 담음 (offstTop활용)
+	- scroll_btns클릭시 new Anime를 이용해서 해당 영역으로 스크롤 모션이동 처리
+	- 11시 40분까지 
+*/
